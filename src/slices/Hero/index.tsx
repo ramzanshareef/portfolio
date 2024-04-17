c8eb4ea61ab7bc30c97e2cdde1588251827dcbcb
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-04-17 20:06:53 +0530

    Update Hero component: Remove animated text and fix aria-label

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -68,7 +68,6 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
                 <Shapes />
                 <div className="col-start-1 md:row-start-1">
-                    <span className="text-4xl animate-pulse opacity-80 text-indigo-500">Hey 👋 I am</span>
                     <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
                         <span className="block text-slate-300">
                             {renderLetters(slice.primary.first_name, "first")}
